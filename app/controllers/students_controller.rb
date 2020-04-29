d20508a816ec7cdd0071233a76a36d04ec6cc5a4
Ryan Mui <reachryanmui@gmail.com>
2020-04-29 00:50:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :activate]
 
+<<<<<<< HEAD
 
   def activate
     @student.active = !@student.active
     @student.save
     redirect_to student_path(@student)
   end
+=======
+  # def active?
+  #   # binding.pry
+  #   @student.active ? 'active' : 'inactive'
+  # end
+
+>>>>>>> 2993c854c2cf8e52e517356bf0c600cbae10ebea
 
   def index
     @students = Student.all
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,19 @@
 class Student < ActiveRecord::Base
 
+
+  # @active = false
   def to_s
     self.first_name + " " + self.last_name
   end
+<<<<<<< HEAD
   def display_active
     active ? "active" : "inactive"
   end
+=======
+  # def is_active
+  #   self.active ? "active" : "inactive"
+  # end
+>>>>>>> 2993c854c2cf8e52e517356bf0c600cbae10ebea
 end
 
 # drop database
